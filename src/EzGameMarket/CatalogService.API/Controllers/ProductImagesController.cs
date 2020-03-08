be6c4d90dec3d4a88e4aafe452d72f9816e395ff
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-08 17:20:08 +0100

    Model manager controllers added

---
+++ src/EzGameMarket/CatalogService.API/Controllers/ProductImagesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using CatalogService.API.Data;
+using CatalogService.API.Models;
+
+namespace CatalogService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductImagesController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductImagesController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProductImages
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductImage>>> GetProductImages()
+        {
+            return await _context.ProductImages.ToListAsync();
+        }
+
+        // GET: api/ProductImages/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<ProductImage>> GetProductImage(int id)
+        {
+            var productImage = await _context.ProductImages.FindAsync(id);
+
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+
+            return productImage;
+        }
+
+        // PUT: api/ProductImages/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProductImage(int id, ProductImage productImage)
+        {
+            if (id != productImage.ID)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(productImage).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductImageExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProductImages
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<ProductImage>> PostProductImage(ProductImage productImage)
+        {
+            _context.ProductImages.Add(productImage);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProductImage", new { id = productImage.ID }, productImage);
+        }
+
+        // DELETE: api/ProductImages/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<ProductImage>> DeleteProductImage(int id)
+        {
+            var productImage = await _context.ProductImages.FindAsync(id);
+            if (productImage == null)
+            {
+                return NotFound();
+            }
+
+            _context.ProductImages.Remove(productImage);
+            await _context.SaveChangesAsync();
+
+            return productImage;
+        }
+
+        private bool ProductImageExists(int id)
+        {
+            return _context.ProductImages.Any(e => e.ID == id);
+        }
+    }
+}
---
+++ src/EzGameMarket/CatalogService.API/Controllers/ProductManagerController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogService.API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CatalogService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductManagerController : ControllerBase
+    {
+        private ProductDbContext _context;
+        private ILogger<ProductManagerController> _logger;
+
+        public ProductManagerController(ProductDbContext context,
+                                        ILogger<ProductManagerController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        
+    }
+}
