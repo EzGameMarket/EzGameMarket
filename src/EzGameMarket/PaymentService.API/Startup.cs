29d96965cabe9a4198df83f0cf923728895b18fa
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-08 22:00:08 +0100

    Swagger and JWT Auth added to PaymentService

--- src/EzGameMarket/PaymentService.API/Startup.cs
+++ src/EzGameMarket/PaymentService.API/Startup.cs
@@ -1,7 +1,9 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +12,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace PaymentService.API
 {
@@ -26,6 +30,34 @@ namespace PaymentService.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            AddJWT(services);
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+        }
+
+        private void AddJWT(IServiceCollection services)
+        {
+            var key = Encoding.UTF8.GetBytes(Configuration.GetValue<string>("Secret"));
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(key),
+                    ValidateIssuer = false,
+                    ValidateAudience = false,
+                    AuthenticationType = "Identity.Application"
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,8 +70,31 @@ namespace PaymentService.API
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
+
             app.UseRouting();
 
+            app.Use(async (context, next) =>
+            {
+                var authenticationCookieName = "access_token";
+                var cookie = context.Request.Cookies[authenticationCookieName];
+                if (cookie != null)
+                {
+                    if (context.Request.Headers.ContainsKey("Authorization") == false)
+                    {
+                        context.Request.Headers.Add("Authorization", "Bearer " + cookie);
+                    }
+                }
+
+                await next();
+            });
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
