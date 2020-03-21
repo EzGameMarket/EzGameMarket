09f00e095bba1ad56b190eeb9c50df912c8072a8
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-21 12:53:06 +0100

    EventBus settings created

---
+++ src/EzGameMarket/EventBuses/EventBus.Shared/Settings/EventBusSettings.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EventBus.Shared.Settings
+{
+    public class EventBusSettings
+    {
+        public static EventBusSettings Settings => LoadSettings();
+
+        public bool AzureServiceBusEnabled { get; set; }
+        public bool AzureStorageEnabled { get; set; }
+        public bool SubscriptionClientName { get; set; }
+        public int EventBusRetryCount { get; set; }
+
+        private static EventBusSettings LoadSettings()
+        {
+            if (Settings == default)
+            {
+                
+            }
+
+            return Settings;
+        }
+    }
+}
