1208caadc840e164369c8e000a4571410d0f98a0
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-23 18:40:04 +0100

    Blazor SPA added

---
+++ src/EzGameMarket/WebApps/WebSPA/Data/SqlDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace WebSPA.Data
+{
+    public class SqlDbContext : DbContext
+    {
+        public SqlDbContext(DbContextOptions<SqlDbContext> options)
+           : base(options)
+        {
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
