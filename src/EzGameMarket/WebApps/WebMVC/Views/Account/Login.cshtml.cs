508171b0df37c705d2ec64e35a7c4133c1130f08
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-23 17:56:32 +0100

    Account views added

---
+++ src/EzGameMarket/WebApps/WebMVC/Views/Account/Login.cshtml.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebMVC.Views.Account
+{
+    public class LoginModel : PageModel
+    {
+        public class InputModel
+        {
+
+        }
+
+
+        public void OnGet()
+        {
+            ViewData["Title"] = "Bejelentkezés";
+        }
+
+        public void OnPost()
+        {
+            
+        }
+    }
+}
