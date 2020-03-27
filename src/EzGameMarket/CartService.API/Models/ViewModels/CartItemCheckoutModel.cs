e096bda98c387287bf893cd1ced83531ad783cd6
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-27 11:33:21 +0100

    checkout model created, and required property attributes added

---
+++ src/EzGameMarket/CartService.API/Models/ViewModels/CartItemCheckoutModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CartService.API.Models.ViewModels
+{
+    public class CartItemCheckoutModel
+    {
+        [Required]
+        [DataType(DataType.ImageUrl)]
+        public string ProductID { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string ProductName { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string ImageUrl { get; set; }
+    }
+}
--- src/EzGameMarket/CartService.API/Models/ViewModels/CheckoutModel.cs
+++ src/EzGameMarket/CartService.API/Models/ViewModels/CheckoutModel.cs
@@ -1,6 +1,46 @@
-﻿namespace CartService.API.Models.ViewModels
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CartService.API.Models.ViewModels
 {
     public class CheckoutModel
     {
+        [Required]
+        [DataType(DataType.PostalCode)]
+        public string PostCode { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string City { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Country { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string Address1 { get; set; }
+        [DataType(DataType.Text)]
+        public string Address2 { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string FirstName { get; set; }
+        [Required]
+        [DataType(DataType.Text)]
+        public string LastName { get; set; }
+        [DataType(DataType.Text)]
+        public string CompanyName { get; set; }
+        [DataType(DataType.Text)]
+        public string VATNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        public string BuyerID { get; set; }
+        [Required]
+        public List<CartItemCheckoutModel> Items { get; set; }
     }
 }
