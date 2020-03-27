285644b023ab48274d4a4e92f55a8107b0c81e2a
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-27 11:35:58 +0100

    Cart checkout views address management created

--- src/EzGameMarket/WebApps/WebMVC/Views/Account/Login.cshtml.cs
+++ src/EzGameMarket/WebApps/WebMVC/Views/Account/Login.cshtml.cs
@@ -1,20 +1,81 @@
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Threading.Tasks;
 
 namespace WebMVC.Views.Account
 {
     public class LoginModel : PageModel
     {
+        [BindProperty]
+        public InputModel Model { get; set; }
+
         public class InputModel
         {
+            [Required]
+            public string EMail { get; set; }
+            [Required]
+            [DataType(DataType.Password)]
+            public string Password { get; set; }
+            [Display(Name = "Emlékezz rám?")]
+            public bool RememberMe { get; set; }
         }
 
-        public void OnGet()
+        private readonly ILogger<LoginModel> _logger;
+
+        public LoginModel(ILogger<LoginModel> logger)
         {
-            ViewData["Title"] = "Bejelentkezés";
+            _logger = logger;
         }
 
-        public void OnPost()
+        [BindProperty]
+        public InputModel Input { get; set; }
+
+        public IList<AuthenticationScheme> ExternalLogins { get; set; }
+
+        public string ReturnUrl { get; set; }
+
+        [TempData]
+        public string ErrorMessage { get; set; }
+
+        public void OnGet(string returnUrl = null)
         {
+            ViewData["Title"] = "Belépés";
+
+            if (!string.IsNullOrEmpty(ErrorMessage))
+            {
+                ModelState.AddModelError(string.Empty, ErrorMessage);
+            }
+
+            ReturnUrl = returnUrl ?? Url.Content("~/");
+        }
+
+        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        {
+            returnUrl = returnUrl ?? Url.Content("~/");
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    
+
+
+                    return LocalRedirect(returnUrl);
+                }
+                catch (Exception ex)
+                {
+
+                }
+            }
+
+            // If we got this far, something failed, redisplay form
+            return Page();
         }
     }
 }
