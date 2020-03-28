d30afb2767ad54b9cce22205b98741796fe283af
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-28 13:19:55 +0100

    PaginationView made shared

---
+++ src/Shared.Extensions.Pagination/PaginationViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shared.Extensions.Pagination
+{
+    public class PaginationViewModel<TEntity> where TEntity : class
+    {
+        public PaginationViewModel(long totalItemsCount, int actualPage, int itemsPerPage, IEnumerable<TEntity> data)
+        {
+            TotalItemsCount = totalItemsCount;
+            ActualPage = actualPage;
+            ItemsPerPage = itemsPerPage;
+            Data = data;
+        }
+
+        public long TotalItemsCount { get; set; }
+        public int ActualPage { get; set; }
+        public int ItemsPerPage { get; set; }
+        public IEnumerable<TEntity> Data { get; set; }
+    }
+}
