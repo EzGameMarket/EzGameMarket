ecdad0504032cf954ac4d4a9ef378cafccd2a68a
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-28 13:25:56 +0100

    CatalogControllers refactored to only catalog and search engine

--- src/EzGameMarket/CatalogService.API/Controllers/CatalogController.cs
+++ src/EzGameMarket/CatalogService.API/Controllers/CatalogController.cs
@@ -1,9 +1,9 @@
-﻿using CatalogService.API.Data;
-using CatalogService.API.Models;
-using CatalogService.API.Models.Pagination;
+﻿using System.Linq;
 using CatalogService.API.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
+using Shared.Extensions.Pagination;
+using CatalogService.API.ViewModels.Product;
 
 namespace CatalogService.API.Controllers
 {
@@ -20,12 +20,19 @@ namespace CatalogService.API.Controllers
 
         [HttpGet]
         [Route("/products")]
-        public async Task<ActionResult<PaginationViewModel<Product>>> GetItems([FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 30)
+        public async Task<ActionResult<PaginationViewModel<CatalogItem>>> GetItems([FromQuery] int pageIndex = 0, [FromQuery] int pageSize = 30)
         {
             var items = await _catalogService.GetItemsAsync(pageIndex*pageSize,pageSize);
             var allCount = await _catalogService.GetAllItemsCount();
 
-            return Ok(new PaginationViewModel<Product>(allCount,pageIndex,pageSize,items));
+            var data = items.Select(p=> new CatalogItem(p.Images.FirstOrDefault(i => i.Size.Size == "Catalog")?.Url ?? "CatalogDefault.jpg",
+                                                        p.GameID,
+                                                        p.Name,
+                                                        p.Price,
+                                                        p.DiscountedPrice,
+                                                        p.Genres.FirstOrDefault()?.Name ?? "Ismeretlen"));
+
+            return Ok(new PaginationViewModel<CatalogItem>(allCount,pageIndex,pageSize, data));
         }
     }
 }
---
+++ src/EzGameMarket/CatalogService.API/Controllers/SearchController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CatalogService.API.Models;
+using CatalogService.API.ViewModels.Product;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Shared.Extensions.Pagination;
+
+namespace CatalogService.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [AllowAnonymous]
+    public class SearchController : ControllerBase
+    {
+        public Task<PaginationViewModel<CatalogItem>> Search(string query)
+        {
+            return default;
+        }
+    }
+}
