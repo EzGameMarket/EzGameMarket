d1401bc2c8e0c7bcd02199127c938d5f99d9aad7
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-28 13:32:25 +0100

    Search model for storing searches created

---
+++ src/EzGameMarket/CatalogService.API/Models/Search.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CatalogService.API.Models
+{
+    public class Search
+    {
+        [Required]
+        [Key]
+        public Guid UserID { get; set; }
+        public string Query { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
