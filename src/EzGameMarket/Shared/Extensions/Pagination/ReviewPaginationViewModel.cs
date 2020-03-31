bd564106e3399541e2bda9c4a6f3f891c2cbad4f
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-03-31 12:43:46 +0200

    Shared projects loaded from Shared/Extensions physical folder

---
+++ src/EzGameMarket/Shared/Extensions/Pagination/ReviewPaginationViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Extensions.Pagination
+{
+    public class ReviewPaginationViewModel<T> : PaginationViewModel<T> where T : class
+    {
+        public ReviewPaginationViewModel(long totalItemsCount, int actualPage, int itemsPerPage, IEnumerable<T> data) : base(totalItemsCount, actualPage, itemsPerPage, data)
+        {
+        }
+
+        //csillagok átlag értéke
+        public double ProductRate { get; set; }
+    }
+}
