55c811f21b7a58ab78ec3546a3c0703bbdca3d53
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-04-13 19:46:25 +0200

    ImageScalerExtension Shared Project created

---
+++ src/EzGameMarket/Shared/Extensions/ImageScaler/Abstractions/IImageResizerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shared.Extensions.ImageScaler.Abstractions
+{
+    public interface IImageResizerService
+    {
+    }
+}
---
+++ src/EzGameMarket/Shared/Extensions/ImageScaler/Abstractions/IImageScalerService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Extensions.ImageScaler.Abstractions
+{
+    public interface IImageScalerService
+    {
+        /// <summary>
+        /// Az image paraméterben megadott képet a times paraméterben megadott constans értékkel skálázza fel
+        /// Ha a times kisebb mint 0 akkor kicsinyíti, ha nagyobb akkor nagyítja, ha pedig pontossan 0 akkor nem változtat a képen
+        /// </summary>
+        /// <param name="image">A kép amit módosítani kell</param>
+        /// <param name="times">A skála, hogy hányszorosra modósítja</param>
+        /// <returns>Vissza adja a módosításokon átesett képet</returns>
+        Task<byte[]> ScaleAsync(byte[] image, int times);
+    }
+}
