893bd65534c8d2a8a66552b9e025f88378ac7e11
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-04-13 20:02:53 +0200

    Uploading images initialized

--- src/EzGameMarket/Services/CatalogImages/CatalogImages.API/Controllers/CatalogItemImagesController.cs
+++ src/EzGameMarket/Services/CatalogImages/CatalogImages.API/Controllers/CatalogItemImagesController.cs
@@ -2,8 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CatalogImages.API.Exceptions.Images.Model;
+using CatalogImages.API.Exceptions.ImageSize.Model;
+using CatalogImages.API.Exceptions.ImageType.Model;
 using CatalogImages.API.Models;
 using CatalogImages.API.Services.Repositories.Abstractions;
+using CatalogImages.API.Services.Service.Abstractions;
 using CatalogImages.API.ViewModels.Image;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,10 +18,13 @@ namespace CatalogImages.API.Controllers
     [ApiController]
     public class CatalogItemImagesController : ControllerBase
     {
-        private ICatalogItemImageRepository _catalogItemImageRepository;
+        private ICatalogItemImageService _catalogItemImageService;
+        private ICatalogImageRepository _catalogItemImageRepository;
 
-        public CatalogItemImagesController(ICatalogItemImageRepository catalogItemImageRepository)
+        public CatalogItemImagesController(ICatalogItemImageService catalogItemImageService,
+                                           ICatalogImageRepository catalogItemImageRepository = default)
         {
+            _catalogItemImageService = catalogItemImageService;
             _catalogItemImageRepository = catalogItemImageRepository;
         }
 
@@ -30,7 +37,7 @@ namespace CatalogImages.API.Controllers
                 return BadRequest();
             }
 
-            var imgs = await _catalogItemImageRepository.GetAllImageForProductID(productID);
+            var imgs = await _catalogItemImageService.GetAllImageForProductID(productID);
 
             if (imgs != default && imgs.Any())
             {
@@ -44,14 +51,14 @@ namespace CatalogImages.API.Controllers
 
         [HttpGet]
         [Route("{productID}/images/filter")]
-        public async Task<ActionResult<List<CatalogItemImageModel>>> GetImagesForProductID([FromRoute] string productID, [FromQuery] string typeName = default, [FromQuery] string sizeName = default)
+        public async Task<ActionResult<List<CatalogItemImageModel>>> GetImagesForProductIDWithFiltering([FromRoute] string productID, [FromQuery] string typeName = default, [FromQuery] string sizeName = default)
         {
             if (string.IsNullOrEmpty(productID))
             {
                 return BadRequest();
             }
 
-            var imgs = await _catalogItemImageRepository.GetAllImageForProductIDByFiltering(productID,typeName, sizeName);
+            var imgs = await _catalogItemImageService.GetAllImageForProductIDByFiltering(productID,typeName, sizeName);
 
             if (imgs != default && imgs.Any())
             {
@@ -60,6 +67,66 @@ namespace CatalogImages.API.Controllers
             else
             {
                 return NotFound();
+            }
+        }
+
+        [HttpPost]
+        [Route("{id}/set/size")]
+        public async Task<ActionResult> ChangeSize([FromRoute] int id, [FromRoute] int sizeID)
+        {
+            if (id <= 0)
+            {
+                return BadRequest($"A megadott kép azonosítója: {id} érvénytelen, nem lehet kisebb egynél");
+            }
+
+            if (sizeID <= 0)
+            {
+                return BadRequest($"A megadott kép méret azonosítója: {sizeID} érvénytelen, nem lehet kisebb egynél");
+            }
+
+            try
+            {
+                await _catalogItemImageRepository.ModifySize(id, sizeID);
+
+                return Ok();
+            }
+            catch (CImageNotFoundException)
+            {
+                return NotFound($"Nem létezik feltöltött kép a {id} azonosítóval");
+            }
+            catch (ImageSizeNotFoundByIDException)
+            {
+                return NotFound($"Nem létezik kép méret a {sizeID} azonosítóval");
+            }
+        }
+
+        [HttpPost]
+        [Route("{id}/set/type")]
+        public async Task<ActionResult> ChangeType([FromRoute] int id, [FromRoute] int typeID)
+        {
+            if (id <= 0)
+            {
+                return BadRequest($"A megadott kép azonosítója: {id} érvénytelen, nem lehet kisebb egynél");
+            }
+
+            if (typeID <= 0)
+            {
+                return BadRequest($"A megadott kép típus azonosítója: {typeID} érvénytelen, nem lehet kisebb egynél");
+            }
+
+            try
+            {
+                await _catalogItemImageRepository.ModifyType(id, typeID);
+
+                return Ok();
+            }
+            catch (CImageNotFoundException)
+            {
+                return NotFound($"Nem létezik feltöltött kép a {id} azonosítóval");
+            }
+            catch (ImageTypeNotFoundByIDException)
+            {
+                return NotFound($"Nem létezik kép típus a {typeID} azonosítóval");
             }
         }
     }
--- src/EzGameMarket/Services/CatalogImages/CatalogImages.API/Controllers/ImagesController.cs
+++ src/EzGameMarket/Services/CatalogImages/CatalogImages.API/Controllers/ImagesController.cs
@@ -3,9 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CatalogImages.API.Models;
+using CatalogImages.API.Services.Repositories.Abstractions;
+using CatalogImages.API.Services.Repositories.Implementations;
+using CatalogImages.API.Services.Service.Abstractions;
 using CatalogImages.API.ViewModels.Image;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Shared.Utilities.CloudStorage.Shared.Services.Abstractions;
 
 namespace CatalogImages.API.Controllers
 {
@@ -13,13 +17,75 @@ namespace CatalogImages.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private ICatalogItemImageService _catalogItemImageService;
+        private ICatalogImageRepository _catalogImageRepository;
+        private IStorageService _storageService;
+
+        public ImagesController(
+            ICatalogItemImageService catalogItemImageService,
+            ICatalogImageRepository catalogImageRepository,
+            IStorageService storageService)
+        {
+            _catalogItemImageService = catalogItemImageService;
+            _catalogImageRepository = catalogImageRepository;
+            _storageService = storageService;
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public async Task<ActionResult<CatalogItemImageModel>> GetByID(int id)
+        {
+            if (id <= 0)
+            {
+                return BadRequest();
+            }
+
+            var img = await _catalogImageRepository.GetByID(id);
+
+            if (img != default)
+            {
+                return img;
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
 
         [HttpPost]
         [Route("upload")]
         //Ellenőrzés, hogy a fájlok amiket fel akar tölteni az valós
         public async Task<ActionResult> Upload([FromBody] UploadNewImageViewModel model)
         {
+
+
+            if (model.Images.Count() > 10)
+            {
+                return BadRequest("Egyszerre max 10 fájlt tölthetsz fel");
+            }
+
+            var tasks = model.Images.Select(async image =>
+            {
+                await ManageUploadedImage(image);
+            });
+
             return default;
+        }
+
+        private async Task ManageUploadedImage(IFormFile image)
+        {
+            //test.png
+            var fileNameWithExtension = image.FileName;
+
+
+            if (image.Length >= CatalogItemImageModel.MaxFileLength)
+            {
+                throw new ApplicationException($"A {fileNameWithExtension} fájl mérete meghaladta a {CatalogItemImageModel.MaxFileLength / 1_048_576.0} MB-ot");
+            }
+
+            using var stream = image.OpenReadStream();
+
+            await _storageService.UploadFromStreamWithID(fileNameWithExtension, stream);
         }
 
         [HttpPost]
