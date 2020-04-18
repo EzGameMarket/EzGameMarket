c26210249ce7248868ed40fad984821e408107d2
Werdnik Krisztián <werdnikkrisz@gmail.com>
2020-04-18 15:33:20 +0200

    Identity service access link added for MVC

--- src/EzGameMarket/WebApps/WebMVC/Extensions/Settings/LoadBalancerUrls.cs
+++ src/EzGameMarket/WebApps/WebMVC/Extensions/Settings/LoadBalancerUrls.cs
@@ -8,5 +8,6 @@ namespace WebMVC.Extensions.Settings
     public class LoadBalancerUrls : ILoadBalancerUrls
     {
         public string MainBalancer { get; set; } = "http://web-gtw:80";
+        public string IdentityService { get; set; } = "http://identity-api:80";
     }
 }
